3c82a5afa5ecfaef7572019644bd853b4df84e4f
matheus pessoa <matheusmpessoa17@gmail.com>
2022-08-08 18:31:15 -0300

    Fix icons and positions css

--- module-federation/projects/mfe1/src/app/app.module.ts
+++ module-federation/projects/mfe1/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { ReportModule } from './reports/reports.module';
 import { APP_ROUTES } from './app.routes';
-import { SharedModule } from 'projects/shared/src/lib/shared.module';
 import { AngularMaterialModule } from 'projects/shared/src/lib/material.module';
-import { SharedService } from 'projects/shared/src/lib/shared.service';
 
 @NgModule({
   imports: [
@@ -16,6 +14,7 @@ import { SharedService } from 'projects/shared/src/lib/shared.service';
     CommonModule,
     ReportModule,
     AngularMaterialModule,
+    MatIconModule,
     RouterModule.forRoot(APP_ROUTES)
   ],
   declarations: [
